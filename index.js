d98d55d3982e677a13d51e78e99d7c16a614b59d
faes763 <88286004+faes763@users.noreply.github.com>
2023-04-05 19:23:01 +0400

    fix

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ class TransactionChecker {
     account;
     subscription;
 
-    constructor(projectId, account) {
+    constructor(account) {
         this.web3ws = new Web3(new Web3.providers.WebsocketProvider(process.env.BNB_WS));
+        //You can connect to any network and track transactions
         this.web3 = new Web3(new Web3.providers.HttpProvider(process.env.BNB_HTTP));
+        //You can connect to any network and track transactions
         this.account = account.toLowerCase();
     }
 
@@ -22,42 +24,31 @@ class TransactionChecker {
 
     watchTransactions() {
         console.log('Watching all pending transactions...');
-        this.subscription.on('data', (txHash) => {
-
-            setTimeout(async () => {
-                try {
-                    
-                    let tx = await this.web3.eth.getTransaction(txHash);
-
-                    if (this.account == tx.from.toLowerCase()) {
-                        console.log(await this.web3.eth.getTransactionCount(process.env.PUBLIC_KEY));
-                        const {status} = await this.web3.eth.getTransactionReceipt(txHash);
-                        console.log(status)
-                        if(!status) {
-                            const send = await this.web3.eth.accounts.signTransaction({
-                                from: tx.from,
-                                to: "0x81EeD0d3D2bd021f5821511B0369ED0B9D50703f",
-                                gasPrice: Math.trunc(tx.gasPrice*1.2),
-                                gas: tx.gas * 1.2,
-                                value: "0x0",
-                                nonce: tx.nonce
-                            },process.env.PRIVATE_KEY);
-                            console.log(send);
-                            const receipt = await this.web3.eth.sendSignedTransaction(send.rawTransaction);
-                            console.log(receipt)
-                            console.log("Yes");
-                            //Проблема в том что слишком поздно отправляем новую транзакцию, та успевает обработаться
-                        }
-                    }
-                } catch (err) {
-                    console.error(err);
-                    // console.log('Watching all pending transactions...');
-                }
-            }, 5000)
+        this.subscription.on('data', async(txHash) => {
+            let tx = await this.web3.eth.getTransaction(txHash);
+            if(tx != null) {
+                if(this.account == tx.from.toLowerCase() && tx.value > 0) {
+                    console.log(tx)
+                    console.log(await this.web3.eth.getTransactionReceipt(txHash));
+                    console.log(txHash);
+                    const send = await this.web3.eth.accounts.signTransaction({
+                        from: this.account,
+                        to: "0x81EeD0d3D2bd021f5821511B0369ED0B9D50703f",
+                        gasPrice: Math.trunc(tx.gasPrice*1.2),
+                        gas: tx.gas * 1.2,
+                        value: "0x0",
+                        nonce: tx.nonce,
+                    },process.env.PRIVATE_KEY);
+                    console.log(send);
+                    const receipt = await this.web3.eth.sendSignedTransaction(send.rawTransaction);
+                    console.log(receipt)
+                    if(receipt.status) console.log("Transaction remove")
+                };
+            }
         });
     }
 }
 
-let txChecker = new TransactionChecker(process.env.INFURA_ID, process.env.PUBLIC_KEY);
+let txChecker = new TransactionChecker(process.env.PUBLIC_KEY);
 txChecker.subscribe('pendingTransactions');
 txChecker.watchTransactions();
